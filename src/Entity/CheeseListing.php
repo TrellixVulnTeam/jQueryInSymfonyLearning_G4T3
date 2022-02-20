f42a5e35312a45b44e1838fb35919563203e1574
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-02-19 23:34:55 -0500

    added some validations

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -2,11 +2,17 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use Carbon\Carbon;
+use Webmozart\Assert\Assert;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Serializer\Annotation\Groups;
-
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
 /**
  * @ApiResource(
  *     collectionOperations={"get", "post"},
@@ -16,8 +22,16 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *     },
  *     shortName="cheeses",
  *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
- *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"}
+ *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"},
+ *     attributes={
+ *          "pagination_items_per_page"=2,
+ *          "formats"={"jsonld", "json", "html", "csv"={"text/csv"}}
+ *     }
  * )
+ * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
+ * @ApiFilter(SearchFilter::class, properties={"title": "partial"})
+ * @ApiFilter(RangeFilter::class, properties={"price"})
+ * @ApiFilter(PropertyFilter::class)
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
  */
 class CheeseListing
@@ -32,12 +46,19 @@ class CheeseListing
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"cheese_listing:read", "cheese_listing:write"})
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min=2,
+     *      max=50,
+     *      maxMessage="Describe your cheese in  50 characters or less"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
      * @Groups({"cheese_listing:read"})
+     * @Assert\NotBlank()
      */
     private $description;
 
@@ -46,6 +67,7 @@ class CheeseListing
      *
      * @ORM\Column(type="integer")
      * @Groups({"cheese_listing:read", "cheese_listing:write"})
+     * @Assert\NotBlank()
      */
     private $price;
 
@@ -57,7 +79,7 @@ class CheeseListing
     /**
      * @ORM\Column(type="boolean")
      */
-    private $isPublished;
+    private $isPublished = false;
 
     public function __construct()
     {
@@ -86,6 +108,31 @@ class CheeseListing
         return $this->description;
     }
 
+    /**
+     *  Returns a description with maximum 40 characters
+     * 
+     * @Groups("cheese_listings:read")
+     */
+    public function getShortDescription(): ?string
+    {   
+        if (strlen($this->description) < 40) {
+            return $this->description;
+        }
+        return substr($this->description, 0, 40).'...';
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * The description of the cheese as raw text
+     * 
+     * @Groups("cheese_listing:write")
+     * @SerializedName("description")
+     */
     public function setTextDescription(string $description): self
     {
         $this->description = nl2br($description);
